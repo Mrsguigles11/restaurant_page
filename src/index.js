a47b5a70890a675aab20065fea9e0c28cceab2b7
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2024-11-12 13:28:11 +0000

    Add a basic layout with three working nav buttons that change the content text.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./styles.css";
+import { homeContent } from "./home.js";
+import { menuContent } from "./menu.js";
+import { aboutContent } from "./about.js";
 
-console.log("HI")
+homeContent();
+
+(function () {
+    const homeButton = document.querySelector("#home");
+
+    homeButton.addEventListener('click', () => {
+        homeContent();
+    })
+})();
+
+(function () {
+    const menuButton = document.querySelector("#menu");
+
+    menuButton.addEventListener('click', () => {
+        menuContent();})
+})();
+
+(function () {
+    const aboutButton = document.querySelector("#about");
+
+    aboutButton.addEventListener('click', () => {
+        aboutContent();
+    })
+})();
