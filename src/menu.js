9d11b912dd606d5b56002b1207113a1f7482043b
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2024-11-16 18:06:56 +0000

    Add a completed menu tab with content and CSS.

--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,38 @@ export {menuContent};
 
 function menuContent () {
     const content = document.querySelector("#content");
-    content.textContent = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-}
+    content.innerHTML = '';
+    const menuContent = document.createElement('div');
+    menuContent.setAttribute('class', 'menu_content');
+    content.append(menuContent);
+    const startersHeader = document.createElement('h1');
+    startersHeader.textContent = "Starters";
+    startersHeader.setAttribute('class', 'menu_header');
+    const startersList = document.createElement('div');
+    startersList.setAttribute('class', 'menu_list');
+    startersList.textContent = "Pumpkin Soup\nJalapeno Poppers\nTraditional Greek Houmous\nGarden Salad";
+    const startersPriceList = document.createElement('div');
+    startersPriceList.setAttribute('class', 'menu_price_list');
+    startersPriceList.textContent = "5\n7\n5\n4";
+    const mainsHeader = document.createElement('h1');
+    mainsHeader.setAttribute('class', 'menu_header');
+    mainsHeader.textContent = "Mains";
+    const mainsList = document.createElement('div');
+    mainsList.setAttribute('class', 'menu_list');
+    mainsList.textContent = "Mushroom Risotto\nVeggie Dumplings W/ Red Kraut\nLasagna\nVeggie Burger\nVegan Fish and Chips\nVegan Currywurst";
+    const mainsPriceList = document.createElement('div');
+    mainsPriceList.setAttribute('class', 'menu_price_list');
+    mainsPriceList.textContent = "10\n10\n12\n11\n13\n12";
+    const dessertsHeader = document.createElement('div');
+    dessertsHeader.setAttribute('class', 'menu_header');
+    dessertsHeader.textContent = "Desserts";
+    const dessertsList = document.createElement('div');
+    dessertsList.setAttribute('class', 'menu_list');
+    dessertsList.textContent = "Gypsy Tart\nSachertorte\nEaton Mess";
+    const dessertsPriceList = document.createElement('div');
+    dessertsPriceList.setAttribute('class', 'menu_price_list');
+    dessertsPriceList.textContent = "7\n8\n6";
 
+    menuContent.append(startersHeader, startersList, startersPriceList, mainsHeader, mainsList, mainsPriceList,
+                        dessertsHeader, dessertsList, dessertsPriceList);
+}
